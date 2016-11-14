746e6d992b7afc3929c10d6a856b8804e9bbc32a
pjy <78237724@qq.com>
2016-11-14 23:34:26 +0800

    2016年11月14日 23:34:26

--- widgets/app.jsx
+++ widgets/app.jsx
@@ -2,13 +2,17 @@ import React, {Component } from 'react';
 import {render } from 'react-dom';
 import { Router, Route, Link } from 'react-router';
 import Name from './components/name.jsx'
+import Nav from './components/navigation.jsx'
 class App extends Component{
 	constructor(props) {
 		super(props);
 	}
 	render(){
 		return (
+			<div>
 			<Name />
+			<Nav />
+			</div>
 			)
 	}
 }
